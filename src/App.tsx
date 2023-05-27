4e5b3e2b50d8ae649ea0238e90365ceb679b6aa0
koubm <koubm@300624.cn>
2023-05-27 16:51:58 +0800

    FIX: 解决mac-scrollbar打包报错问题

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import HomeRoute from './routers';
 import { BrowserRouter } from "react-router-dom";
 import Provider from './redux/store';
 import { GlobalScrollbar } from 'mac-scrollbar';
-import 'mac-scrollbar/dist/mac-scrollbar.css';
+// 应该要引用库中的 css 文件，即
+// import 'mac-scrollbar/dist/mac-scrollbar.css'
+// 但是该 css 文件中有语法兼容性问题，导致 build 报错
+// 故将 css 文件单独拷贝出来并作修改，再引用
+import './mac-scrollbar.css';
 
 const IsPC = () => {
   const userAgentInfo = navigator.userAgent;
