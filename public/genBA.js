3cbdb7fe3d774a4a62211218056e6f8b592d21a2
Bester <71936924+bmmmmmmmmm@users.noreply.github.com>
2024-05-01 23:45:02 +0800

    ADD: kou.ink (#5)

--- public/genBA.js
+++ public/genBA.js
@@ -3,10 +3,11 @@ window.onload = () => {
     const BAMap = new Map([
       ['koubenming.com', ['豫ICP备2021020491号-1']],
       ['www.koubenming.com', ['豫ICP备2021020491号-1']],
+      ['kbm.ink', ['豫ICP备2021020491号-2']],
       ['www.kbm.ink', ['豫ICP备2021020491号-2']],
-      // ['kou.ink', ['豫ICP备2021020491号-3']],
-      // ['bester.kou.ink', ['豫ICP备2021020491号-3']],
-      // ['www.kou.ink', ['豫ICP备2021020491号-3']],
+      ['kou.ink', ['豫ICP备2021020491号-3']],
+      ['www.kou.ink', ['豫ICP备2021020491号-3']],
+      ['bester.kou.ink', ['豫ICP备2021020491号-3']],
       // ['bester.cc', ['豫ICP备2021020491号-4']],
       // ['www.bester.cc', ['豫ICP备2021020491号-4']],
     ])
